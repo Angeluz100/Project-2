d92388e4d5b6c648d93e020632f3b268a177265c
Angeluz100 <francotgutierrez@gmail.com>
2021-03-25 09:34:05 -0400

    fix: adds env var access for port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const methodOverride = require ('method-override')
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 require('dotenv').config();
 require("./config/database");
